ceba7d6814c583b1ef4290847f86d7bfc4d79462
nurdauletzhuzbay <nurdaulet.zhuzbay@nu.edu.kz>
2024-12-23 12:22:03 +0500

    commit

--- rpi-esp.py
+++ rpi-esp.py
@@ -38,7 +38,11 @@ def send_nano_command(command):
 def send_movement_command(direction, distance):
     global current_pos_x, current_pos_y, current_pos_z
     cmd = ""
-
+    data = read_esp32_data()
+    if data:
+        current_pos_x, current_pos_y, current_pos_z = data
+        print(current_pos_x, current_pos_y, current_pos_z)
+        
     if direction == "forward":
         target_pos_x = current_pos_x + distance
         cmd = f"AK80,{target_pos_x:.4f},{current_pos_y:.4f},{current_pos_z:.4f}"
@@ -244,8 +248,5 @@ def interactive_control():
 
 if __name__ == "__main__":
     interactive_control()
-    data = read_esp32_data()
-    if data:
-        current_pos_x, current_pos_y, current_pos_z = data
-        print(current_pos_x, current_pos_y, current_pos_z)
+
 
