8463ed988ff348ffa51926c868886526161b2291
nurdauletzhuzbay <nurdaulet.zhuzbay@nu.edu.kz>
2024-12-24 12:23:27 +0500

    port

--- rpi-esp.py
+++ rpi-esp.py
@@ -1,8 +1,8 @@
 import serial
 import time
 
-ESP32_PORT = '/dev/ttyUSB0'
-ARDUINO_PORT = '/dev/ttyUSB1'
+ESP32_PORT = '/dev/ttyUSB1'
+ARDUINO_PORT = '/dev/ttyUSB0'
 BAUD_RATE_ESP = 38400
 BAUD_RATE_NANO = 9600
 TIMEOUT = 1
