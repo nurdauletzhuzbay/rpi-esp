f51064341fb28b1c333a6919870fd0702fe44ebf
nurdauletzhuzbay <nurdaulet.zhuzbay@nu.edu.kz>
2025-01-17 17:34:15 +0500

    changes

--- rpi-esp.py
+++ rpi-esp.py
@@ -120,12 +120,12 @@ def change_chassis(chassis_command, esp32_serial):
 def read_esp32_data():
     try:
         response = esp32_serial.readline().decode('utf-8').strip()
+        print(f"RAW {response}")
         return parse_esp32_data(response)
     except serial.SerialException as e:
         print(f"Serial error: {e}")
     except Exception as e:
         print(f"Unexpected error: {e}")
-    return None
 
 
 def parse_esp32_data(response):
